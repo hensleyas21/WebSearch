d1228fd8a496352483f2c30f000e168a3836f11e
hensleyas21 <hensleyas21@gcc.edu>
2025-03-13 13:48:37 -0400

    rankedPages should be working

--- src/main/scala/AskWillie.scala
+++ src/main/scala/AskWillie.scala
@@ -16,7 +16,7 @@ import scala.util.Sorting
         val pages: Map[String, WebPage] = mapWebPages(loadWebPages()) // completed for you
 
         // TODO: Measure the importance of each page using one of the functions in PageRank
-        val rankedPages: List[RankedWebPage] = PageRank.equal(pages) // call PageRank.???? here
+        val rankedPages: List[RankedWebPage] = PageRank.equal(pages).map((id, weight) => new RankedWebPage(pages(id), weight)).toList // call PageRank.???? here
 
         // Get user input then perform search until ":quit" is entered
         var query: String = ""
