70204647796cb2691b39f8c23d1a9cc3b95cb6ef
hensleyas21 <hensleyas21@gcc.edu>
2025-03-13 13:26:38 -0400

    PageSearch count

--- src/main/scala/PageSearch.scala
+++ src/main/scala/PageSearch.scala
@@ -8,7 +8,9 @@ object PageSearch {
      * @return       a list of the number of times any of the terms appeared in each page in the same order as given
      */
     def count(pages: List[RankedWebPage], query: List[String]): List[Double] = {
-        List() // TODO: implement this method and remove this stub
+        for page <- pages yield {
+            (for term <- query yield page.text.sliding(term.length).count(_ == term)).sum.toDouble
+        }
     }
 
     /**
